be6c7415ecefba9e72053fc52114e5c6fd030ae8
Humansharma <97686915+Humansharma@users.noreply.github.com>
2022-06-03 01:23:49 +0530

    Update Signin_app.js

--- Signin_app.js
+++ Signin_app.js
@@ -2,23 +2,7 @@ const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
 
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
-});
 
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
-const mysql=require("mysql");
-
-const connection =mysql.createConnection({
-	host:"localhost",
-	user: "root",
-	password:"123",
-	database:"node.js"
-});
-connection.connect(function(error){
-	if(error)throw error
-	else console.log("connected to the database successfully!")
-	
-})
